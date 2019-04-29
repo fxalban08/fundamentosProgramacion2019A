0ff959862dd1d2eb1d8f650c779a40305594f0fa
knitaz <knitazepn@hotmail.com>
2019-04-29 11:57:23 -0500

    Update Hola mundo cout.cpp

--- Hola mundo cout.cpp
+++ Hola mundo cout.cpp
@@ -3,6 +3,7 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
+	cout<<"Bienvenido";
 	cout<<"Hola Mundo";
 	return 0;
 }
